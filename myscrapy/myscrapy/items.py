f2a804c41d546ff29ac0da4206c95291ad49f110
demeen68 <demeen68@gmail.com>
2017-12-28 21:05:09 +0800

    Update items.py

--- myscrapy/myscrapy/items.py
+++ myscrapy/myscrapy/items.py
@@ -13,4 +13,4 @@ from save_app.models import My_save_model
 class MyscrapyItem(DjangoItem):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    save_model = My_save_model
+    django_model = My_save_model
